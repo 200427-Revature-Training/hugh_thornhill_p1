c1d230622de8a35d06b60b15e9234a1f26ea697b
hughthorn <hlowerthornhill@gmail.com>
2020-06-16 16:09:15 -0400

    Added logout to welcome page

--- client/src/components/home.component.tsx
+++ client/src/components/home.component.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
+import { useHistory } from 'react-router';
 
 const HomeComponent: React.FC = () => {
+    const history = useHistory();
+    const logOutButton = () => {
+        localStorage.clear();
+        history.push('/');
+      }
 
     return (
         <div>
             <h1>Welcome!</h1>
+            <form>
+            <button type = "submit" onClick = {() => logOutButton()}>
+                Log Out
+            </button>
+            </form>
         </div>
     );
 }
